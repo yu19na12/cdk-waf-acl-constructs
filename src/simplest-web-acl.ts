4ae2a117c150c39423e7afad4dab6644d18ef204
kaji <nakajima.subjob@gmail.com>
2024-07-24 23:50:41 +0900

    変更

--- src/simplest-web-acl.ts
+++ src/simplest-web-acl.ts
@@ -1,16 +1,22 @@
 import { Construct } from 'constructs';
 import { aws_wafv2 as wafv2 } from 'aws-cdk-lib';
 
-export interface ComprehensiveWebAclProps {
+export interface simplestWebAclProps {
   name: string;
   scope: 'CLOUDFRONT' | 'REGIONAL';
+  region: string;
 }
 
-export class ComprehensiveWebAcl extends Construct {
-  constructor(scope: Construct, id: string, props: ComprehensiveWebAclProps) {
+export class SimplestWebAcl extends Construct {
+  constructor(scope: Construct, id: string, props: simplestWebAclProps) {
     super(scope, id);
 
-    new wafv2.CfnWebACL(this, 'ComprehensiveWebAcl', {
+    let waf_region = props.region
+    if ( props.scope = "CLOUDFRONT" ){
+      waf_region = "us-east-1"
+    }
+
+    new wafv2.CfnWebACL(this, 'SimplestWebAcl', {
       name: props.name,
       scope: props.scope,
       defaultAction: { allow: {} },
